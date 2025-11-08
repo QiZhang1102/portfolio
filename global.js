fce1f4f40b7f52fd16deb9bb703ba953d22f88cf
QiZhang1102 <zq20021102mia@gmail.com>
2025-11-07 16:20:01 -0800

    1

--- global.js
+++ global.js
@@ -10,6 +10,7 @@ let pages = [
   { url: 'projects/', title: 'Projects' },
   { url: 'contact/', title: 'Contact' },
   { url: 'resume/', title: 'Resume' },
+  { url: 'meta/', title: 'Meta' }, 
   { url: 'https://github.com/QiZhang1102',title: 'GitHub' },
 ];
 
