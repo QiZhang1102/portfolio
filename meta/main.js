9d3414537e30ac51ccf77d77666a4438ec0073b4
QiZhang1102 <zq20021102mia@gmail.com>
2025-11-21 21:40:07 -0800

    4

--- meta/main.js
+++ meta/main.js
@@ -1,10 +1,14 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
+let commitProgress = 100;
+let timeScale = null;
+let filteredCommits = null;
+let commitMaxTime = null;
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
 let xScale, yScale;
 function createBrushSelector(svg) {
   svg.call(d3.brush());
 }
-
-
 
 async function loadData() {
   const data = await d3.csv('loc.csv', (row) => ({
@@ -25,7 +29,6 @@ function processCommits(data) {
     .map(([commit, lines]) => {
       let first = lines[0];
       let { author, date, time, timezone, datetime } = first;
-
       let ret = {
         id: commit,
         url: "https://github.com/QiZhang1102/portfolio/commit/" + commit,
@@ -84,7 +87,7 @@ function renderTooltipContent(commit) {
   const link = document.getElementById('commit-link');
   const date = document.getElementById('commit-date');
   const author = document.getElementById('commit-author');
-  const time = document.getElementById('commit-time');
+  const time = document.getElementById('tooltip-time');
   const lines = document.getElementById('commit-lines');
 
   if (!commit || Object.keys(commit).length === 0) return;
@@ -208,7 +211,6 @@ function renderScatterPlot(data, commits) {
     .range([2, 30]);
 
   const sortedCommits = d3.sort(commits, d => -d.totalLines);
-
   const margin = { top: 10, right: 10, bottom: 30, left: 40 };
   const usableArea = {
     left: margin.left,
@@ -236,13 +238,17 @@ function renderScatterPlot(data, commits) {
       .tickSize(-usableArea.width)
   );
 
-  svg.append('g')
-    .attr('transform', `translate(0, ${usableArea.bottom})`)
-    .call(xAxis);
+  svg
+  .append('g')
+  .attr('transform', `translate(0, ${usableArea.bottom})`)
+  .attr('class', 'x-axis')
+  .call(xAxis);
 
-  svg.append('g')
-    .attr('transform', `translate(${usableArea.left}, 0)`)
-    .call(yAxis);
+svg
+  .append('g')
+  .attr('transform', `translate(${usableArea.left}, 0)`)
+  .attr('class', 'y-axis')
+  .call(yAxis);
 
   svg.call(
     d3.brush().on('start brush end', brushed)
@@ -252,7 +258,7 @@ function renderScatterPlot(data, commits) {
   const dots = svg.append('g').attr('class', 'dots');
 
   dots.selectAll('circle')
-    .data(sortedCommits)
+    .data(sortedCommits, (d) => d.id)
     .join('circle')
     .attr('cx', d => xScale(d.datetime))
     .attr('cy', d => yScale(d.hourFrac))
@@ -273,6 +279,123 @@ function renderScatterPlot(data, commits) {
 
 let data = await loadData();
 let commits = processCommits(data);
+filteredCommits = commits;
+
+timeScale = d3.scaleTime()
+  .domain([
+    d3.min(commits, d => d.datetime),
+    d3.max(commits, d => d.datetime),
+  ])
+  .range([0, 100]);
+
+commitMaxTime = timeScale.invert(commitProgress);
 
 renderCommitInfo(data, commits);
 renderScatterPlot(data, commits);
+updateFileDisplay(filteredCommits);
+
+
+
+function updateScatterPlot(data, commits) {
+  const width = 1000;
+  const height = 600;
+  const margin = { top: 10, right: 10, bottom: 30, left: 40 };
+
+  const usableArea = {
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left,
+    width: width - margin.left - margin.right,
+    height: height - margin.top - margin.bottom,
+  };
+
+  const svg = d3.select('#chart').select('svg');
+
+  xScale.domain(d3.extent(commits, d => d.datetime));
+  xScale.range([usableArea.left, usableArea.right]);
+  yScale.range([usableArea.bottom, usableArea.top]);
+
+  const [minLines, maxLines] = d3.extent(commits, d => d.totalLines);
+  const rScale = d3.scaleSqrt().domain([minLines, maxLines]).range([2, 30]);
+
+  const xAxis = d3.axisBottom(xScale);
+  const xAxisGroup = svg.select('g.x-axis');
+  xAxisGroup.selectAll('*').remove();
+  xAxisGroup.call(xAxis);
+
+  const dots = svg.select('g.dots');
+  const sortedCommits = d3.sort(commits, d => -d.totalLines);
+
+  dots
+    .selectAll('circle')
+    .data(sortedCommits, (d) => d.id)
+    .join('circle')
+    .attr('cx', d => xScale(d.datetime))
+    .attr('cy', d => yScale(d.hourFrac))
+    .attr('r', d => rScale(d.totalLines))
+    .attr('fill', 'steelblue')
+    .style('fill-opacity', 0.7)
+    .on('mouseenter', (event, commit) => {
+      d3.select(event.currentTarget).style('fill-opacity', 1);
+      renderTooltipContent(commit);
+      updateTooltipVisibility(true);
+      updateTooltipPosition(event);
+    })
+    .on('mouseleave', (event) => {
+      d3.select(event.currentTarget).style('fill-opacity', 0.7);
+      updateTooltipVisibility(false);
+    });
+}
+
+function updateFileDisplay(filteredCommits) {
+  let lines = filteredCommits.flatMap((d) => d.lines);
+  let files = d3
+  .groups(lines, (d) => d.file)
+  .map(([name, lines]) => ({ name, lines }))
+  .sort((a, b) => b.lines.length - a.lines.length);
+
+  let filesContainer = d3
+    .select('#files')
+    .selectAll('div')
+    .data(files, (d) => d.name)
+    .join(
+      (enter) =>
+        enter.append('div').call((div) => {
+          div.append('dt').append('code');
+          div.append('dd');
+        })
+    );
+  filesContainer
+    .select('dt')
+    .html((d) => `<code>${d.name}</code><small>${d.lines.length} lines</small>`);
+  filesContainer
+  .select('dd')
+  .selectAll('div')
+  .data((d) => d.lines)
+  .join('div')
+  .attr('class', 'loc')
+  .attr("style", (d) => `--color: ${colors(d.type)}`);
+}
+
+function onTimeSliderChange() {
+  const slider = document.getElementById("commit-progress");
+  const timeEl = document.getElementById("commit-time");
+
+  commitProgress = Number(slider.value);
+  commitMaxTime = timeScale.invert(commitProgress);
+
+  timeEl.textContent = commitMaxTime.toLocaleString(undefined, {
+    dateStyle: "long",
+    timeStyle: "short",
+  });
+  filteredCommits = commits.filter((d) => d.datetime <= commitMaxTime);
+  updateScatterPlot(data, filteredCommits);
+  updateFileDisplay(filteredCommits);
+}
+
+const slider = document.getElementById("commit-progress");
+slider.addEventListener("input", onTimeSliderChange);
+slider.addEventListener("change", onTimeSliderChange);
+
+onTimeSliderChange();
